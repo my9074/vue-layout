cda24b2ab5d2bfb3fc122bfcee7fe3b2d8950644
my9074 <313231068@qq.com>
2017-05-14 20:35:46 +0800

    Add style.css file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <nav-menu></nav-menu>
-    <router-view></router-view>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
